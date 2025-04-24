b6081b86e111c482361afc0c2887519112920df7
Luis Perugachi <luisprg05@gmail.com>
2025-04-24 12:03:21 -0500

    fix: improved auth.ts to login, logout with credentials

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,50 +1,54 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import NextAuth, { type NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import { checkUserCredentials } from '@/actions/auth';
 import prisma from '@/lib/prisma';
-import { signInEmailPassword } from '@/actions/auth';
 
 export const authConfig: NextAuthConfig = {
+	pages: {
+		signIn: '/auth/login',
+	},
 	providers: [
 		Credentials({
 			name: 'Credentials',
 			credentials: {
-				// name: { label: 'Name', type: 'text', placeholder: 'Luis' },
-				email: { label: 'Email', type: 'email', placeholder: 'example@google.com' },
+				email: { label: 'Email', type: 'email', placeholder: 'example@mail.com' },
 				password: { label: 'Password', type: 'password' },
 			},
-			authorize: async (credentials, req) => {
-				// const name = credentials.name as string;
-				const name = '';
+			authorize: async credentials => {
 				const email = credentials.email as string;
 				const password = credentials.password as string;
 
-				const user = await signInEmailPassword(email, name, password);
+				const user = await checkUserCredentials(email, password);
 
-				return user;
+				if (!user) return null;
+
+				const { password: _, ...restUser } = user;
+
+				return restUser;
 			},
 		}),
 	],
 	session: { strategy: 'jwt' },
 	callbacks: {
-		signIn: async ({ user, account, profile, email, credentials }) => {
-			return true;
-		},
-		jwt: async ({ token, user, profile, account }) => {
-			const dbUser = await prisma.user.findUnique({ where: { email: token.email ?? 'no-email' } });
+		jwt: async ({ token }) => {
+			const dbUser = await prisma.user.findUnique({
+				where: { email: token.email ?? 'no-email' },
+				include: { organization: true },
+			});
 
 			if (!dbUser) throw new Error('User not found');
 
 			token.id = dbUser?.id ?? 'no-uuid';
+			token.organizationId = dbUser?.organizationId ?? 'no-uuid';
+			token.organizationName = dbUser?.organization?.name ?? 'no-name';
 
 			return token;
 		},
-		session: async ({ session, token, user }) => {
+		session: async ({ session, token }) => {
 			if (!session || !session.user) return session;
 
 			session.user.id = token.id;
-			session.user.roles = token.roles;
+			session.user.organizationId = token.organizationId;
 
 			return session;
 		},
